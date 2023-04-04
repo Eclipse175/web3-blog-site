831cd6c1c295dc0fdaabfac1b25819c88a4e7068
Eclipse175 <fahimkhobar2004@gmail.com>
2023-04-04 08:03:00 +0300

    completed the navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Navbar/Navbar.js';
+import Header from './Header/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../App.js';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className = 'header'>
+            This is the Header component
+        </header>
+    )
+}
+export default Header;
